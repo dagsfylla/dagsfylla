00a6b028d5810fb52a32401f536b61f25b2fe94b
ankile <lars.lien@ignite.no>
2019-03-04 08:32:01 +0100

    Some initial grommet testing

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Box, Button, Collapsible, Heading, Grommet, Layer, ResponsiveContext } from 'grommet';
+import { FormClose, Notification } from 'grommet-icons';
+
+const theme = {
+    global: {
+        colors: {
+            brand: '#e62256',
+        },
+        font: {
+            family: 'Roboto',
+            size: '14px',
+            height: '20px',
+        },
+    },
+};
+
+const AppBar = props => (
+    <Box
+        tag="header"
+        direction="row"
+        align="center"
+        justify="between"
+        background="brand"
+        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+        elevation="medium"
+        style={{ zIndex: '1' }}
+        {...props}
+    />
+);
+
+class App extends Component {
+    state = {
+        showSideBar: false,
+    };
+
+    render() {
+        let { showSidebar } = this.state;
+        return (
+            <Grommet theme={theme} full>
+                <ResponsiveContext.Consumer>
+                    {size => (
+                        <Box>
+                            <AppBar>
+                                <Heading level="3" margin="none">
+                                    My App
+                                </Heading>
+                                <Button
+                                    icon={<Notification />}
+                                    onClick={() =>
+                                        this.setState(prevState => ({ showSidebar: !prevState.showSidebar }))
+                                    }
+                                />
+                            </AppBar>
+                            <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                                <Box flex align="center" justify="center">
+                                    app body
+                                </Box>
+                                {!showSidebar || size !== 'small' ? (
+                                    <Collapsible direction="horizontal" open={showSidebar}>
+                                        <Box
+                                            flex
+                                            width="medium"
+                                            background="light-2"
+                                            elevation="small"
+                                            align="center"
+                                            justify="center"
+                                        >
+                                            sidebar
+                                        </Box>
+                                    </Collapsible>
+                                ) : (
+                                    <Layer>
+                                        <Box
+                                            background="light-2"
+                                            tag="header"
+                                            justify="end"
+                                            align="center"
+                                            direction="row"
+                                        >
+                                            <Button
+                                                icon={<FormClose />}
+                                                onClick={() => this.setState({ showSidebar: false })}
+                                            />
+                                        </Box>
+                                        <Box fill background="light-2" align="center" justify="center">
+                                            sidebar
+                                        </Box>
+                                    </Layer>
+                                )}
+                            </Box>
+                        </Box>
+                    )}
+                </ResponsiveContext.Consumer>
+            </Grommet>
+        );
+    }
+}
+
+export default App;
