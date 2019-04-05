dee94f36c65e7475b4c84d3d1dff2a338a215ba3
ankile <lars.lien@ignite.no>
2019-04-05 20:56:43 +0200

    Add basic crud for events

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import LandingPage from './containers/LandingPage';
 import UserPage from "./containers/UserPage";
-import TestForm from "./containers/TestPage";
-import TestGetUsername from "./containers/TestPage/TestGetUsername";
 
 const theme = {
     global: {
@@ -28,10 +26,8 @@ class App extends Component {
                 <Grommet theme={theme} full>
                     <Box fill align="center" height="100vh">
                         <Switch>
-                            <Route exact path={'/'} component={LandingPage} />
-                            <Route exact path={'/test-fauna'} component={TestForm} />
-                            <Route path={'/test-fauna/:username'} component={TestGetUsername} />
-                            <Route path={'/:username'} component={UserPage} />
+                            <Route exact path={'/'} component={LandingPage}/>
+                            <Route path={'/:username'} component={UserPage}/>
                         </Switch>
                     </Box>
 
