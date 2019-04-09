36c61e4e13ce591fcd4f59a5fa9142ebdb2ddcd1
Martinegeli <martinegeli9@gmail.com>
2019-04-09 15:37:30 +0200

    Initial design done after comments from Lars. Functionality should be implemented for MVP

--- src/containers/UserPage/DetailView.js
+++ src/containers/UserPage/DetailView.js
@@ -46,62 +46,60 @@ class DetailView extends React.Component {
         */
         return (
             <Row style={{marginTop: 20}}>
-                <Col xs="9">
-                    <Container>
-                        <Row style={{margin: 25}}>
-                            <Col sm="12" md={{ size: 6, offset: 3 }} style={{textAlign: "center"}}>
-                                <Heading>{detail.name}</Heading>
-                            </Col>
-                        </Row>
-                        <Row style={{margin: 25}}>
-                            <Col style={{textAlign: "center"}}>
-                                <BigClock date={date} />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col sm="9">
-                                <Card style={{height: 338}}>
-                                    <CardHeader><h4>Beskrivelse</h4></CardHeader>
-                                    <CardBody>
-                                        <CardText style={{textAlign: "center"}}><i>{detail.description}</i></CardText>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col sm="3">
-                                <Card style={{height: 338}}>
-                                    <CardHeader><h4>Deltagere</h4></CardHeader>
-                                    <Row>
-                                        <Col>
-                                            <Input type="text" name="participants" placeholder="skriv navn her .." />
-                                        </Col>
-                                        <Col sm="auto">
-                                            <Button>Legg til</Button>
-                                        </Col>
-                                    </Row>
-                                    <CardBody>
-                                        {participants.map((participant, i) => (
-                                            <Row>
-                                                <p>{participant}</p>
-                                            </Row>
-                                        ))}
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-                </Col>
-                <Col xs="3">
-                    <Card style={{margin: 20, height: 600}}>
-                        <CardHeader><h4>Praktisk info</h4></CardHeader>
-                        <CardBody>
-                            <CardText>Adresse: <i>{detail.address}</i></CardText>
-                            <CardText>Type fylla: <i>{detail.type}</i></CardText>
-                            <CardText>Dato: <i>{detail.date}</i></CardText>
-                            <CardText>Tidspunkt: <i>{detail.date}</i></CardText>
-                            <CardText>Antall: <i>{detail.maxPeople}</i></CardText>
-                        </CardBody>
-                    </Card>
-                </Col>
+                <Container>
+                    <Row style={{margin: 25}}>
+                        <Col sm="12" md={{ size: 6, offset: 3 }} style={{textAlign: "center"}}>
+                            <Heading>{detail.name}</Heading>
+                        </Col>
+                    </Row>
+                    <Row style={{margin: 25}}>
+                        <Col style={{textAlign: "center"}}>
+                            <BigClock date={date} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="6">
+                            <Card style={{height: 400}}>
+                                <CardHeader><h4>Beskrivelse</h4></CardHeader>
+                                <CardBody>
+                                    <CardText style={{textAlign: "center"}}><i>{detail.description}</i></CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col sm="3">
+                            <Card style={{height: 400}}>
+                                <CardHeader><h4>Deltagere</h4></CardHeader>
+                                <Row>
+                                    <Col>
+                                        <Input type="text" name="participants" placeholder="skriv navn her .." />
+                                    </Col>
+                                    <Col sm="auto">
+                                        <Button>Legg til</Button>
+                                    </Col>
+                                </Row>
+                                <CardBody>
+                                    {participants.map((participant, i) => (
+                                        <Row>
+                                            <p>{participant}</p>
+                                        </Row>
+                                    ))}
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col xs="3">
+                            <Card style={{height: 400}}>
+                                <CardHeader><h4>Praktisk info</h4></CardHeader>
+                                <CardBody>
+                                    <CardText>Adresse: <i>{detail.address}</i></CardText>
+                                    <CardText>Type fylla: <i>{detail.type}</i></CardText>
+                                    <CardText>Dato: <i>{detail.date}</i></CardText>
+                                    <CardText>Tidspunkt: <i>{detail.date}</i></CardText>
+                                    <CardText>Antall: <i>{detail.maxPeople}</i></CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </Row>
         );
     }
