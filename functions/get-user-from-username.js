f4de5b8e1da0c7451aa6a85d13e47f99e75e0534
ankile <lars.lien@ignite.no>
2019-04-09 18:34:10 +0200

    Pasted a fix in the file

--- functions/get-user-from-username.js
+++ functions/get-user-from-username.js
@@ -4,7 +4,7 @@ const q = faunadb.query;
 const client = new faunadb.Client({
     secret: process.env.FAUNADB_SECRET,
 });
-
+// If(IsEmpty(Paginate(Match(Index('user_by_username'), 'noe'))), Create(Class('users'), {'data': {"username": "noe"}}), Get(Match(Index('user_by_username'), 'noe')))
 exports.handler = (event, context, callback) => {
     console.log('in function get-user-from-username');
     const data = JSON.parse(event.body);
