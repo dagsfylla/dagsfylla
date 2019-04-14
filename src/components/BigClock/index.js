67a73682fddae933149b121e8d66546f04373c16
ankile <lars.lien@ignite.no>
2019-04-14 21:59:39 +0200

    fix: Add several small fixes

--- src/components/BigClock/index.js
+++ src/components/BigClock/index.js
@@ -1,58 +1,67 @@
 import React, { Component } from 'react';
 
 import { StyledClock } from './style';
-import differenceInSeconds from 'date-fns/difference_in_seconds'
+import differenceInSeconds from 'date-fns/difference_in_seconds';
 
 import { Spinner } from 'reactstrap';
 
 const second = 1,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
 class BigClock extends Component {
     constructor(props) {
         super(props);
         this.state = {
             loading: true,
-         }
+        };
     }
 
     componentDidMount() {
         let { date } = this.props;
-        this.interval = setInterval(() => (
-            this.setState({
-                seconds: differenceInSeconds(new Date(date), new Date()),
-                loading: false,
-            })
-        ), 1000);
+        this.interval = setInterval(
+            () =>
+                this.setState({
+                    seconds: differenceInSeconds(new Date(date), new Date()),
+                    loading: false,
+                }),
+            1000
+        );
     }
 
     componentWillUnmount() {
         clearInterval(this.interval);
     }
 
-    render() { 
-        let distance = this.state.seconds
+    render() {
+        let distance = this.state.seconds;
 
-        return ( 
+        return (
             <div>
-                {this.state.loading
-                ?
-                <Spinner color="primary" style={{height: 145, width: 145}}/>
-                :
+                {this.state.loading ? (
+                    <Spinner color="primary" style={{ height: 145, width: 145 }} />
+                ) : (
                     <StyledClock>
                         <ul>
-                            <li><span id="days">{Math.floor(distance / (day))}</span>dager</li>
-                            <li><span id="hours">{Math.floor(distance % (day) / (hour))}</span>timer</li>
-                            <li><span id="minutes">{Math.floor(distance % (hour) / (minute))}</span>minutter</li>
-                            <li><span id="seconds">{Math.floor(distance % (minute) / (second))}</span>sekunder</li>
+                            <li>
+                                <span id="days">{Math.floor(distance / day)}</span>dager
+                            </li>
+                            <li>
+                                <span id="hours">{Math.floor((distance % day) / hour)}</span>timer
+                            </li>
+                            <li>
+                                <span id="minutes">{Math.floor((distance % hour) / minute)}</span>minutter
+                            </li>
+                            <li>
+                                <span id="seconds">{Math.floor((distance % minute) / second)}</span>sekunder
+                            </li>
                         </ul>
                     </StyledClock>
-                }
+                )}
             </div>
-         );
+        );
     }
 }
- 
+
 export default BigClock;
--- src/containers/UserPage/DetailView.js
+++ src/containers/UserPage/DetailView.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 import BigClock from '../../components/BigClock/index';
 import { CardBodyScroll } from './style';
 
 import { Heading } from 'grommet';
-import { Container, Row, Col, Card, CardText, Input, CardHeader, Button, CardBody } from 'reactstrap';
+import { Card, CardHeader, CardText, Col, Container, Row } from 'reactstrap';
 
 class DetailView extends React.Component {
     render() {
@@ -14,96 +15,91 @@ class DetailView extends React.Component {
             },
         } = this.props;
 
-        console.log('id', id);
-        console.log('events', this.props.events);
-
         let event = this.props.events.find(e => e.ref === id);
+
+        if (!event) {
+            return null;
+        }
+
+        let [date, time] = format(new Date(event.starttime), 'DD-MM-YYYY HH:mm').split(' ');
 
         return (
             <Row style={{ marginTop: 20, marginLeft: 0, marginRight: 0 }}>
-                {event ? (
-                    <Container>
-                        <Row style={{ margin: 25 }}>
-                            <Col sm="12" md={{ size: 6, offset: 3 }} style={{ textAlign: 'center' }}>
-                                <Heading>{event.name}</Heading>
-                            </Col>
-                        </Row>
-                        <Row style={{ margin: 25 }}>
-                            <Col style={{ textAlign: 'center' }}>
-                                <BigClock date={event.starttime} />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="6" sm="12">
-                                <Card style={{ height: 400 }}>
-                                    <CardHeader>
-                                        <h4>Beskrivelse</h4>
-                                    </CardHeader>
-                                    <CardBodyScroll>
-                                        <CardText style={{ textAlign: 'center' }}>
-                                            <i>{event.description}</i>
-                                        </CardText>
-                                    </CardBodyScroll>
-                                </Card>
-                            </Col>
-                            <Col lg="3" sm="6">
-                                <Card style={{ height: 400 }}>
-                                    <CardHeader>
-                                        <h4>Deltagere</h4>
-                                    </CardHeader>
-                                    <Row>
-                                        <Col style={{ paddingRight: 0 }}>
-                                            <Input
-                                                type="text"
-                                                name="participants"
-                                                placeholder="skriv navn her .."
-                                            />
-                                        </Col>
-                                        <Col xs="auto" style={{ paddingLeft: 0 }}>
-                                            <Button>Legg til</Button>
-                                        </Col>
-                                    </Row>
-                                    <CardBodyScroll>
-                                        {event.participants
-                                            ? event.participants.map((participant, i) => (
-                                                  <Row>
-                                                      <p>{participant}</p>
-                                                  </Row>
-                                              ))
-                                            : null}
-                                    </CardBodyScroll>
-                                </Card>
-                            </Col>
-                            <Col lg="3" sm="6">
-                                <Card style={{ height: 400 }}>
-                                    <CardHeader>
-                                        <h4>Praktisk info</h4>
-                                    </CardHeader>
-                                    <CardBodyScroll>
-                                        <CardText>
-                                            Adresse: <i>{event.address}</i>
-                                        </CardText>
-                                        <CardText>
-                                            Type fylla: <i>{event.type}</i>
-                                        </CardText>
-                                        <CardText>
-                                            Dato: <i>{event.date}</i>
-                                        </CardText>
-                                        <CardText>
-                                            Tidspunkt: <i>{event.time}</i>
-                                        </CardText>
-                                        <CardText>
-                                            Antall: <i>{event.maxPeople}</i>
-                                        </CardText>
-                                        <CardText>
-                                            Byensjef: <i>Tanigan</i>
-                                        </CardText>
-                                    </CardBodyScroll>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-                ) : null}
+                <Container>
+                    <Row style={{ margin: 25 }}>
+                        <Col sm="12" md={{ size: 6, offset: 3 }} style={{ textAlign: 'center' }}>
+                            <Heading>{event.name}</Heading>
+                        </Col>
+                    </Row>
+                    <Row style={{ margin: 25 }}>
+                        <Col style={{ textAlign: 'center' }}>
+                            <BigClock date={event.starttime} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg="6" sm="12">
+                            <Card style={{ height: 400 }}>
+                                <CardHeader>
+                                    <h4>Beskrivelse</h4>
+                                </CardHeader>
+                                <CardBodyScroll>
+                                    <CardText style={{ textAlign: 'center' }}>
+                                        <i>{event.description}</i>
+                                    </CardText>
+                                </CardBodyScroll>
+                            </Card>
+                        </Col>
+                        {/* Commented out because it is not yet implemented */}
+                        {/*<Col lg="3" sm="6">*/}
+                            {/*<Card style={{ height: 400 }}>*/}
+                                {/*<CardHeader>*/}
+                                    {/*<h4>Deltagere</h4>*/}
+                                {/*</CardHeader>*/}
+                                {/*<Row>*/}
+                                    {/*<Col style={{ paddingRight: 0 }}>*/}
+                                        {/*<Input type="text" name="participants" placeholder="skriv navn her .." />*/}
+                                    {/*</Col>*/}
+                                    {/*<Col xs="auto" style={{ paddingLeft: 0 }}>*/}
+                                        {/*<Button>Legg til</Button>*/}
+                                    {/*</Col>*/}
+                                {/*</Row>*/}
+                                {/*<CardBodyScroll>*/}
+                                    {/*{event.participants*/}
+                                        {/*? event.participants.map(participant => (*/}
+                                              {/*<Row key={participant}>*/}
+                                                  {/*<p>{participant}</p>*/}
+                                              {/*</Row>*/}
+                                          {/*))*/}
+                                        {/*: null}*/}
+                                {/*</CardBodyScroll>*/}
+                            {/*</Card>*/}
+                        {/*</Col>*/}
+                        <Col lg="6" sm="6">
+                            <Card style={{ height: 400 }}>
+                                <CardHeader>
+                                    <h4>Praktisk info</h4>
+                                </CardHeader>
+                                <CardBodyScroll>
+                                    <CardText>
+                                        Sted: <i>{event.location}</i>
+                                    </CardText>
+                                    <CardText>
+                                        Type fylla: <i>{event.type}</i>
+                                    </CardText>
+                                    <CardText>
+                                        Dato: <i>{date}</i>
+                                    </CardText>
+                                    <CardText>
+                                        Tidspunkt: <i>{time}</i>
+                                    </CardText>
+                                    <CardText>
+                                        Antall: <i>{event.maxPeople}</i>
+                                    </CardText>
+                                </CardBodyScroll>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </Row>
         );
     }
